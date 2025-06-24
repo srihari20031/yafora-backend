f3ba63d1c952191969913b93032e7913fa61b329
srihari20031 <sriharivelayutham.28@gmail.com>
2025-06-24 22:52:01 +0530

    Cookies test

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { signIn, signUp, signOut, testCookies } from '../controller/userController';
+import { signIn, signUp, signOut } from '../controller/userController';
 
 const router = Router();
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.post('/signout', signOut);
-router.get('/test-cookies', testCookies);
 
 export default router;
